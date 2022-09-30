ef5a78dc0b84fcc878b239410a76a16be3c711a2
mcu10166035 <114201423+mcu10166035@users.noreply.github.com>
2022-09-30 11:46:19 +0800

    week04

---
+++ week04-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    /// K & R
+    for (int i=0; i<10; i++) {
+        printf("印出 : %d\n",i);
+    }
+    for (int i=1; i<=10; i++) {
+        printf("人類的版本，印出 : %d\n",i);
+    }
+}
