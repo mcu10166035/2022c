14bc2b6b51e525b56cbf010afaccfaae8b41d0d6
mcu10166035 <mcu10166035@me.mcu.edu.tw>
2022-12-16 11:30:34 +0800

    week15

---
+++ week15/week15-2.cpp
@@ -0,0 +1,17 @@
+///ASCII介紹
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%c : %d\n",65,65);
+    printf("%c : %d\n",66,66);
+    printf("%c : %d\n",67,67);
+    printf("%c : %d\n",'A','A');
+    printf("%c : %d\n",'B','B');
+    printf("%c : %d\n",'C','C');
+    printf("上面用數字 65 及單引號'a' 的結果一致\n");
+    printf("%c : %d\n",97,97);
+    printf("%c : %d\n",'a','a');
+}
